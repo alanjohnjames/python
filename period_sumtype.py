b3afb0938e3d06ebcd394f82102fe4f712be8d34
alanjohnjames <alanjohnjames@gmail.com>
2017-09-11 23:23:26 +0100

    CHANGE: insert some line breaks

--- period_sumtype.py
+++ period_sumtype.py
@@ -25,16 +25,20 @@ class PeriodSumtype(object):
 @match(PeriodSumtype)
 class print_period(object):
     def DurationPeriod(start_date, duration):
-        return "start_date: {0}\n  duration: {1}".format(str(start_date), str(duration)),
+        return "start_date: {0}\n  duration: {1}".format(
+            str(start_date), str(duration)),
     def DatePeriod(start_date, end_date):
-        return "start_date: {0}\n  end_date: {1}".format(str(start_date), str(end_date))
+        return "start_date: {0}\n  end_date: {1}".format(
+            str(start_date), str(end_date))
 
 start = date(2017, 7, 28)
 end = date(2017, 8, 28)
 duration = "1M"
 
-print("DurationPeriod:\n{}".format(print_period(PeriodSumtype.DurationPeriod(start, duration))))
-print("DatePeriod:\n{}".format(print_period(PeriodSumtype.DatePeriod(start, end))))
+print("DurationPeriod:\n{}".format(
+    print_period(PeriodSumtype.DurationPeriod(start, duration))))
+print("DatePeriod:\n{}".format(
+    print_period(PeriodSumtype.DatePeriod(start, end))))
 
 print("Finished...")
 
