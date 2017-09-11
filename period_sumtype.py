ca8e11a0a5f069fbdd938d8cb43020d8edf35a67
alanjohnjames <alanjohnjames@gmail.com>
2017-09-11 23:11:24 +0100

    UPDATE: small pep8 changes

--- period_sumtype.py
+++ period_sumtype.py
@@ -1,13 +1,15 @@
 """ Python Algebraic Data Types (ADTs)
 
     `Period` example from `Stupid Python Ideas`
-    Implemented in using SumTypes Pytohn package
+    Implemented in using SumTypes Python package
 """
 
-# %%
-import attr
+# %%
 from datetime import date
 from sumtypes import sumtype, constructor, match
+
+import attr
+
 
 @sumtype
 class PeriodSumtype(object):
@@ -18,11 +20,12 @@ class PeriodSumtype(object):
 # type datePeriod = end_date * start_date
 # type Period = durationPeriod | datePeriod
 
+
 # type printPeriod = Period -> String
 @match(PeriodSumtype)
 class print_period(object):
     def DurationPeriod(start_date, duration):
-        return "start_date: {0}\n  duration: {1}".format(str(start_date), str(duration)), 
+        return "start_date: {0}\n  duration: {1}".format(str(start_date), str(duration)),
     def DatePeriod(start_date, end_date):
         return "start_date: {0}\n  end_date: {1}".format(str(start_date), str(end_date))
 
@@ -30,31 +33,29 @@ start = date(2017, 7, 28)
 end = date(2017, 8, 28)
 duration = "1M"
 
-# assert get_num(MyType.NamedNum('foo', 1)) == 1
-# assert get_num(MyType.AnonymousNum(2)) == 2
-
 print("DurationPeriod:\n{}".format(print_period(PeriodSumtype.DurationPeriod(start, duration))))
 print("DatePeriod:\n{}".format(print_period(PeriodSumtype.DatePeriod(start, end))))
 
 print("Finished...")
 
-# %%
 
+# %%
 @sumtype
 class PeriodAttrtype(object):
     DurationPeriod = constructor(
-        start_date = attr.ib(validator=attr.validators.instance_of(date)),
-        duration = attr.ib(validator=attr.validators.instance_of(str)))
+        start_date=attr.ib(validator=attr.validators.instance_of(date)),
+        duration=attr.ib(validator=attr.validators.instance_of(str)))
     DatePeriod = constructor(
-        start_date = attr.ib(validator=attr.validators.instance_of(date)),
-        end_date = attr.ib(validator=attr.validators.instance_of(date)))
+        start_date=attr.ib(validator=attr.validators.instance_of(date)),
+        end_date=attr.ib(validator=attr.validators.instance_of(date)))
 
 @sumtype
 class Period(object):
     Sumtype = constructor(
-        sum_type = attr.ib(validator=attr.validators.instance_of(PeriodSumtype)))
+        sum_type=attr.ib(validator=attr.validators.instance_of(PeriodSumtype)))
     Attrtype = constructor(
-        attr_type = attr.ib(validator=attr.validators.instance_of(PeriodAttrtype)))
+        attr_type=attr.ib(validator=attr.validators.instance_of(PeriodAttrtype)))
+
 
 # type matchPeriod = Period -> Period
 @match(Period)
