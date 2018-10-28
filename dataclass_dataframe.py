7cce6781a241f850b59f7c2e3139e82d9564f694
Alan James <alanjohnjames@users.noreply.github.com>
2018-10-28 14:51:32 +0000

    ADD: from dataframe to dataclass

--- dataclass_dataframe.py
+++ dataclass_dataframe.py
@@ -15,13 +15,13 @@ https://github.com/pandas-dev/pandas/issues/21910
 """
 
 import pandas as pd
-# import pytest
+import pytest
 from dataclasses import dataclass
 from dataclasses import asdict
 
 
 @dataclass
-class SimpleDataObject(object):
+class SimpleDataObject:
     field_a: int
     field_b: str
 
@@ -29,15 +29,16 @@ dataclass_object1 = SimpleDataObject(1, 'a')
 dataclass_object2 = SimpleDataObject(2, 'b')
 
 
-# pytest.skip(reason="Expected Behaviour: Dataclasses to DataFrame.")
+@pytest.mark.skip(reason="Expected Behaviour: Dataclasses to DataFrame.")
 def test_dataclass_to_dataframe():
     """Dataclasses to DataFrame."""
-    df = pd.DataFrame(data=[dataclass_object1, dataclass_object2])
+    df = pd.DataFrame(data=objects)
     df.dtypes == ['field_a', 'field_b']
     df.dtypes == ['int', 'str']
 
 
-# pytest.skip(reason="Expected Behaviour: DataFrame to Dataclasses.")
+@pytest.mark.skip(reason="Expected Behaviour: DataFrame to Dataclasses.")
+# pytest.skip(reason="")
 def test_dataframe_to_dataclass():
     """DataFrame to Dataclasses."""
     df = pd.DataFrame(columns=['field_a', 'field_b'], data=[[1, 'a'], [2, 'b']])
@@ -46,21 +47,40 @@ def test_dataframe_to_dataclass():
 
 
 @dataclass
-class SimpleDataObject(object):
+class SimpleDataObject:
     field_a: int
     field_b: str
 
 
 x = SimpleDataObject(field_a=2, field_b='f')
 
+assert x == SimpleDataObject(field_a=2, field_b='f')
 
-df = pd.DataFrame([asdict(x) for x in [dataclass_object1, dataclass_object2]])
 
-print(df)
+def test_objects_to_dataframe():
 
-"""   field_a field_b
-0        1       a
-1        2       b
-"""
+    dataclass_objects = [dataclass_object1, dataclass_object2]
 
-print("finished...")
+    df = pd.DataFrame([asdict(x) for x in dataclass_objects])
+
+    print(df)
+
+    """   field_a field_b
+    0        1       a
+    1        2       b
+    """
+
+    assert True
+
+
+def test_dataframe_to_objects():
+
+    dataclass_objects = [dataclass_object1, dataclass_object2]
+
+    df = pd.DataFrame([asdict(x) for x in dataclass_objects])
+
+    records = df.to_dict(orient='records')
+
+    record_objects = [SimpleDataObject(*rec.values()) for rec in records]
+
+    assert True
