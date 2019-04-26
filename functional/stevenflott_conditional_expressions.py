4408e6e8fced595d7f1f8067e1695643c21bd50f
Alan James <alanjohnjames@users.noreply.github.com>
2019-04-26 20:01:44 +0100

    UPDATE stevenflott conditional expressions

--- functional/stevenflott_conditional_expressions.py
+++ functional/stevenflott_conditional_expressions.py
@@ -4,6 +4,7 @@ Chapter 13: Conditional Expressions and the Operator Module
 
 http://xlb.es/Functional%20Python%20Programming%20-%20Steve%20Lott%20-%202015.pdf
 
+
 """
 
 #%%
@@ -24,7 +25,13 @@ def fact(n):
 
 fact(5)
 
-#%%
+#%% [markdown]
+# # Exploiting non-strict dictionary rules
+
+# A dictionary's keys have no order. If we try to create a dictionary with multiple items that share a common key value, we'll only have one item in the resulting dict object. It's not clear which of the duplicated key values will be preserved, and it shouldn't matter.
+
+# Here's a situation where we explicitly don't care which of the duplicated keys is preserved. We'll look at a degenerate case of the max() function, it simply picks the largest of two values:
+
 def max(a, b):
     f = {a >= b: lambda: a,
          b >= a: lambda: b}[True]
@@ -32,10 +39,19 @@ def max(a, b):
 
 max(5, 6)
 
-#%%
-# # Filtering true conditional expressions
-# We have a number of ways of determining which expression is True. In the previous example, we loaded the keys into a dictionary. Because of the way the dictionary is loaded, only one value will be preserved with a key of True.
+#%% [markdown]
+
+# In the case where `a == b`, both items in the dictionary will have a key of the `True` condition. Only one of the two will actually be preserved. Since the answer is the same, it doesn't matter which is kept and which is treated as a duplicate and overwritten.
+
+
+#%% [markdown]
+# # Filtering true conditional expressions
+
+# We have a number of ways of determining which expression is `True`. In the previous example, we loaded the keys into a dictionary. Because of the way the dictionary is loaded, only one value will be preserved with a key of `True`.
+
 # Here's another variation to this theme, written using the filter() function:
+
+#%%
 from operator import itemgetter
 
 def semifact(n):
@@ -50,4 +66,7 @@ semifact(10)
 
 #%% [markdown]
 # # Using the operator module instead of lambdas
+# This is markdown body...
 
+
+#%%
